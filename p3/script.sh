dfd1898809a3bea3e6dbe9bc79dbaaf650969a1f
Pierre Beylouni <pbeyloun@f2r9s5.clusters.42paris.fr>
2025-10-20 17:04:48 +0200

    script update

--- p3/script.sh
+++ p3/script.sh
@@ -36,3 +36,12 @@ kubectl create namespace argocd
 # install argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+# install argocd cli
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+# port forwarding
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+# TODO install argocd cli
