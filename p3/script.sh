7a3811bff3e1c0ca72663f22c43ebaa06e71c810
Pierre Beylouni <pbeyloun@f2r9s5.clusters.42paris.fr>
2025-10-21 14:17:56 +0200

    added to script

--- p3/script.sh
+++ p3/script.sh
@@ -36,12 +36,23 @@ kubectl create namespace argocd
 # install argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+# port forwarding
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+
 # install argocd cli
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 
-# port forwarding
-kubectl port-forward svc/argocd-server -n argocd 8080:443
+# get admin argocd password
+export ARGOCD_PASSWORD=$(argocd admin initial-password -n argocd | sed -n '1p')
 
-# TODO install argocd cli
+# login to argocd
+argocd login localhost:8080 \
+    --username admin \
+    --password $ARGOCD_PASSWORD \
+    --insecure
+
+# create application
+argocd app create guestbook --repo https://github.com/argoproj/argocd-example-apps.git --path guestbook --dest-server https://kubernetes.default.svc --dest-namespace default
+
